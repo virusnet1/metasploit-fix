17873eff01e655de55df720e6b44b2217c57f824
Virus Net <107586172+virusnet1@users.noreply.github.com>
2022-11-25 04:32:34 +0000

    Add files via upload

---
+++ metasploit-fix.sh
@@ -0,0 +1,28 @@
+#!bin/bash
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+clear
+#<---virusnet fix metasploit---->
+echo -e $red '                   __                   __      _ __     _____     '
+sleep 0.3
+echo -e $green '   ____ ___  ___  / /_____ __________  / /___  (_) /_   / __(_)  __'
+sleep 0.3
+echo -e $yellow '  / __ `__ \/ _ \/ __/ __ `/ ___/ __ \/ / __ \/ / __/  / /_/ / |/_/'
+sleep 0.3
+echo -e $red ' / / / / / /  __/ /_/ /_/ (__  ) /_/ / / /_/ / / /_   / __/ />  < ' 
+sleep 0.3
+echo -e $yellow '/_/ /_/ /_/\___/\__/\__,_/____/ .___/_/\____/_/\__/  /_/ /_/_/|_|'  
+sleep 0.3
+echo -e $green '                             /_/                                '
+printf "\n"
+sleep 0.3
+echo -e $blue "  By virusnet https://github.com/virusnet1       "
+printf "\n"
+apt autoremove metasploit-framework
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+  chmod 777 msfinstall && \
+  ./msfinstall
+  rm msfinstall
+  msfconsole
